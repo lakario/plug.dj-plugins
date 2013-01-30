01e7d68b3ec3a41bff730774db8dd82e083dfa02
Nathan Taylor <lakario@msn.com>
2013-01-29 18:18:22 -0800

    Fixed bug with emoting

--- resources/bypass-chat-filters.js
+++ resources/bypass-chat-filters.js
@@ -1,9 +1,10 @@
 var chat = function (a) {
 	var that = Models.chat;
 	if (!that.chatCommand(a)) {
+		var type = a.indexOf("/em") == 0 || a.indexOf("/me") == 0 ? 'emote' : 'message';
 		var c = {
-			message: Utils.cleanTypedString(a)
-			, type: a.indexOf("/em") == 0 || a.indexOf("/me") == 0 ? 'emote' : 'message'
+			message: Utils.cleanTypedString(type == 'emote' ? a.substring(3) : a)
+			, type: type
 			, language: Models.user.data.language
 			, from: Models.user.data.username
             , fromID: Models.user.data.id
